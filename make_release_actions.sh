a2f5ed9b40007676513c7b0b84517681d0469a26
Rafael Martins <rafael@arvore.com.br>
2024-11-22 13:22:31 -0300

    Set up Docker Buildx

--- make_release_actions.sh
+++ make_release_actions.sh
@@ -4,15 +4,18 @@ set -e
 COMMIT_HASH=$1
 IMAGE_NAME="ohrafaelmartins/elixirdemo"
 
-# Enable Docker BuildKit for better caching
+# Enable Docker BuildKit
 export DOCKER_BUILDKIT=1
 
-# Build the Docker image with caching
-docker build \
-  --cache-from=${IMAGE_NAME}:latest \
-  --cache-from=${IMAGE_NAME}:${COMMIT_HASH} \
+# Create a builder if it doesn't exist
+docker buildx create --use || true
+
+# Build Docker image with cache from Docker Hub
+docker buildx build \
+  --cache-from=type=registry,ref=${IMAGE_NAME}:latest \
+  --cache-from=type=registry,ref=${IMAGE_NAME}:${COMMIT_HASH} \
   --tag ${IMAGE_NAME}:${COMMIT_HASH} \
   --tag ${IMAGE_NAME}:latest \
-  .
+  --push .  # Push directly to Docker Hub
 
-echo "Docker image ${IMAGE_NAME}:${COMMIT_HASH} built successfully."
+echo "Docker image ${IMAGE_NAME}:${COMMIT_HASH} built and pushed successfully."
